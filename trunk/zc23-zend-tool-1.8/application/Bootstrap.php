a0898b0ff26991e6a1d686645c6d456283b2194c
turkeybastingemus <turkeybastingemus@0d3b21be-13bc-11de-babb-ddc7aea8d8a1>
2009-05-29 14:31:14 +0000

    Companion code to episode 23 of Zendcasts, available free at zendcasts.com

---
+++ trunk/zc23-zend-tool-1.8/application/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+	protected function _initViewHelpers()
+	{
+		$this->bootstrap('layout');		
+		
+		$layout = $this->getResource('layout');
+		
+		$view = $layout->getView();
+		$view->doctype("XHTML1_STRICT");
+
+		$view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+		
+		
+		
+	}
+	
+	
+}
+
