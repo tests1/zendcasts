0de6159a65ac267493bb8cee2f480e777a652d7f
turkeybastingemus <turkeybastingemus@0d3b21be-13bc-11de-babb-ddc7aea8d8a1>
2009-06-02 23:56:06 +0000

    episode 24, introduction to Zend_Navigation

---
+++ trunk/zc24-zend-navigation-breadcrumbs-sitemap-menu/application/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+		$uri = $this->_request->getPathInfo();    	
+		$activeNav = $this->view->navigation()->findByUri($uri);
+		$activeNav->active = true;
+		$activeNav->setClass("active");			
+		
+    }
+
+    public function indexAction()
+    {
+  
+    }
+	public function aboutAction()
+	{
+		
+	}
+	public function contactAction()
+	{
+		
+	}
+	public function sitemapAction()
+	{
+		$this->view->layout()->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+		echo $this->view->navigation()->sitemap();
+		
+	}
+
+}
+
