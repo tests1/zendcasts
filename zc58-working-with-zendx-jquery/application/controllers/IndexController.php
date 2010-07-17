a75e14942e9811f02ad72f115e2219f7de4eb5ff
turkeybastingemus <turkeybastingemus@0d3b21be-13bc-11de-babb-ddc7aea8d8a1>
2010-07-17 22:16:02 +0000

    episode 58 of Zendcasts

---
+++ zc58-working-with-zendx-jquery/application/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+    }
+    public function indexAction()
+    {
+        
+    }
+
+
+}
+
