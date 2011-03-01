4ae2802411bee631f44d3c2266a3e8555344c7ee
turkeybastingemus <turkeybastingemus@0d3b21be-13bc-11de-babb-ddc7aea8d8a1>
2011-03-01 20:37:44 +0000

    episode 69 of Zendcasts

---
+++ trunk/zc69-one-to-many-with-doctrine-2/library/ZC/Entity/Purchase.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ZC\Entity;
+/**
+ * @Table(name="purchases")
+ * @Entity
+ */
+class Purchase
+{
+    /**
+     *
+     * @var integer $id
+     * @Column(name="id", type="integer",nullable=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     *
+     * @var datetime
+     * @Column(type="datetime", nullable=false)
+     */
+    private $created;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string") 
+     */
+    private $itemName;
+
+    /**
+     *
+     * @var decimal
+     *
+     * @Column(type="decimal",precision=2,scale=4)
+     */
+    private $amount;
+
+    /**
+     *
+     * @var User
+     * @ManyToOne(targetEntity="User")
+     * @JoinColumns({
+     *  @JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime(date("Y-m-d H:i:s"));
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public function __set($property,$value)
+    {
+        $this->$property = $value;
+    }
+
+}
+
