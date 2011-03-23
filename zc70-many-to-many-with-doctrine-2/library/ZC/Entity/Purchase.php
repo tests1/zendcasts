4006dde700739dc82e1a35f8b6f81702e059de43
turkeybastingemus <turkeybastingemus@0d3b21be-13bc-11de-babb-ddc7aea8d8a1>
2011-03-23 19:54:06 +0000

    episode 70 of zendcasts

---
+++ zc70-many-to-many-with-doctrine-2/library/ZC/Entity/Purchase.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ZC\Entity;
+/**
+ * @Table(name="purchases")
+ * @Entity
+ */
+class Purchase
+{
+    /**
+     *
+     * @var integer $id
+     * @Column(name="id", type="integer",nullable=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     *
+     * @var datetime
+     * @Column(type="datetime", nullable=false)
+     */
+    private $created;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string") 
+     */
+    private $storeName;
+
+    /**
+     *
+     * @var decimal
+     *
+     * @Column(type="decimal",precision=2,scale=4)
+     */
+    private $amount = 0;
+
+    /**
+     *
+     * @var User
+     * @ManyToOne(targetEntity="User")
+     * @JoinColumns({
+     *  @JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+
+    /**
+     * @ManyToMany(targetEntity="Product", inversedBy="purchases", cascade={"persist,remove"})
+     * @JoinTable(name="purchases_products",
+     *  joinColumns={@JoinColumn(name="purchase_id", referencedColumnName="id")},
+     *  inverseJoinColumns={@JoinColumn(name="product_id",referencedColumnName="id")})
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime(date("Y-m-d H:i:s"));
+        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public function __set($property,$value)
+    {
+        $this->$property = $value;
+    }
+
+}
+
